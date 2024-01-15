17bd9e8f4c2e60e28a6957bc63f5a8fb3c86efe7
Simon-Manasse <simo0047@hz.nl>
2024-01-15 13:06:42 +0100

    changed init to not generate auth file

--- init.sh
+++ init.sh
@@ -13,10 +13,21 @@ function readInFile() {
 }
 
 function addAuth(){
-    if [ ! -f "getCurrentSession.js" ]
-        then
-           callFileText="export async function getCurrentSession(supabase, req) {
+           callFileText="
+import * as dotenv from "'"'"dotenv"'"'";
+
+import { createClient} from "'"'"@supabase/supabase-js"'"'";
+
+dotenv.config({ path: 'variables.env' });
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+export async function getCurrentSession(req) {
+    
             const cookies = req.headers.cookie;
+            if (cookies == null) return { code: 1, error: "'"'"cookies error"'"'"};
             const access_token = cookies.split('; ')[0].split('=')[1];
 
             const refresh_token = cookies.split('; ')[1].split('=')[1];
@@ -29,7 +40,7 @@ function addAuth(){
 
                 console.error('session error', sessionError);
 
-                throw sessionError;
+                return { code: 1, error: "'"'"supabaseError"'"'"};
 
             }
 
@@ -38,12 +49,62 @@ function addAuth(){
                 data: { user },
 
             } = await supabase.auth.getUser();
-        return supabase;
-}";
+        return { code:0, client: supabase};
+}
+
+export async function getAnimals(req, res) {
+  const session = await getCurrentSession(req);
+  if (session['code'] == 1) res.send(\`error in session: \${session['error']}\`);
+  else {
+    const supabaseInstance = session['client'];
+    
+    if (error) res.send(\`query error in supabase: \${error.message}\`);
+    else {
+      res.set("'"'"Access-Control-Allow-Credentials"'"'", "'"'"true"'"'");
+      res.set("'"'"Access-Control-Allow-Origin"'"'", "'"'"http://localhost:5173"'"'");
+      res.status(200).json(data); 
+    } 
+  }
+}
+export async function getDataFrom(req, res, tableName, id = null) {
+  const {data, error} = await chooseData(supabase, tableName, id);
+  sendToClient(res, await data, await error);
+}
+
+export async function getAuthDataFrom(req, res, tableName, id = null) {
+  const session = await getCurrentSession(req);
+  if (session['code'] == 1) res.send(\`error in session: \${session['error']}\`);
+  else {
+    const supabaseInstance = session['client'];
+    const {data, error} = await chooseData(supabaseInstance, tableName, id);
+    sendToClient(res, await data, await error, true)
+  }
+}
+
+async function chooseData(supabaseInstance, tableName, id) {
+    if (id == null) {
+        return await supabaseInstance.from(tableName).select();
+    } else {
+        return await supabaseInstance.from(tableName).select().eq("'"'"id"'"'", id).single();
+    }
+}
+function sendToClient(res, data, error, isAuth = false)
+{
+    
+    if (error) res.send(\`query error in supabase: \${error.message}\`);
+    else {
+        if (isAuth) {
+            res.set("'"'"Access-Control-Allow-Credentials"'"'", "'"'"true"'"'");
+            res.set("'"'"Access-Control-Allow-Origin"'"'", "'"'"http://localhost:5173"'"'");
+        }
+        res.status(200).json(data); 
+    } 
+}
+";
 
 echo -e "$callFileText" > getCurrentSession.js 
 
-        fi
+
 }
 
 
@@ -63,13 +124,11 @@ for folder in ./*; do
         then
             cd "$folder"
                 addEnv
-                addAuth
             cd ..
         else
         cd "$folder"
         cd "code"
         addEnv
-        addAuth
         cd ../..
         fi
     fi
